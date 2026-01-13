92eed1223ff42b33160b608d41ec82c75020521d
dungemeister <yuriy.vazha@gmail.com>
2026-01-13 21:20:08 +0400

    Refactored bot api key

--- main.cpp
+++ main.cpp
@@ -6,8 +6,16 @@
 int main(int argc, char* argv[]){
     std::cout << "Hello from telegram bot. PID: " << getpid() << std::endl;
     
-    TgBot bot("8109080221:AAGk8RTXkouAgvTpPAcpQGyCxuiyeSI1t14");
-    // TgBot bot("8109080221:AAGk8RTXkouAgvTpPAcpQGyCxuiyeSI1t15");
+    std::string api_filepath = "api.key";
+    std::string api_key;
+    std::fstream api_key_file(api_filepath);
+    if(!api_key_file.is_open()){
+        std::cerr <<"fail to open " << api_filepath << std::endl;
+        exit(-1);
+    }
+    std::getline(api_key_file, api_key);
+
+    TgBot bot(api_key);
 
     bot.loop();
 }
